d6f10e7b21ed066850c0417a42e2b92034817c61
geekkid1 <captain.emily.blum@gmail.com>
2025-03-30 11:13:33 -0400

    Finish search-by-date query to avoid potentially hitting date objects in other metadata fields and add application properties fields that allow for file persistence.

--- src/main/java/org/example/restdb/FeedbackRepository.java
+++ src/main/java/org/example/restdb/FeedbackRepository.java
@@ -4,12 +4,16 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public interface FeedbackRepository extends CrudRepository<FeedbackData, Long> {
     List<FeedbackData> findByProductName(String productName);
     List<FeedbackData> findByContent(String content);
 
-    @Query("select f from FeedbackData f where :date member of f.metaData")
+    @Query("select f from FeedbackData f where f.metaData['date'] = :date")
     List<FeedbackData> findByDate(String date);
+
+    @Query("select key(entry) from FeedbackData f join f.metaData as entry")
+    List<String> getMetadata();
 }
